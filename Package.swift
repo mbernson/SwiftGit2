882f2792ce52d1ac0b5824dfe0488c0e0e16ac19
Mathijs Bernson <mathijs.bernson@gmail.com>
2025-02-21 20:07:40 +0100

    Silence clang integer conversion warning

--- Package.swift
+++ Package.swift
@@ -76,7 +76,7 @@ let package = Package(
                   // Disable -fmodules flag. Clang finds (`struct entry`) in a different file (`search.h`).
                   "-fno-modules",
                   // Disable warning: "implicit conversion loses integer precision"
-                  "-Wno-single-bit-bitfield-constant-conversion",
+                  "-Wno-implicit-int-conversion",
                   // Disable warning: "a function definition without a prototype is deprecated"
                   "-Wno-deprecated-non-prototype",
                 ]),
